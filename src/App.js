94538f7ba1dfb8c36d9a9cb4d79ade50522e4f78
heieidi <heidi.p.hennessy@gmail.com>
2019-11-20 14:31:03 +0200

    Home-komponenttiin luotu karuselli. Kaipaa CSS:ää

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import "./App.css";
 import Navigation from "./layout/Navigation";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import TopicForm from './components/TopicForm';
+import Home from './components/Home';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navigation title={"Koodifoorumi"} />
- {/*        <Switch>
+         {/* <Switch> */}
           <Route path="/" exact component={Home}></Route>
-          <Route path="/topics" exact component={TopicList}></Route> */}
+          {/* <Route path="/topics" exact component={TopicList}></Route> */}
           <Route path="/topicsform" component={TopicForm}></Route>
 {/*           <Route path="/topics/:id" component={SingleTopic}></Route>
           <Route component={NotFound}></Route>
---
+++ src/components/Images.js
@@ -0,0 +1,5 @@
+export const images = [
+    "https://memeshappen.com/media/created/Did-i-really-write-this-code--meme-35113.jpg",
+    "https://i.imgflip.com/1nc438.jpg",
+    "https://media.makeameme.org/created/change-one-line.jpg"
+];
