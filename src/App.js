80eb042e3d41af1d8d986d8ca3a8a57182633598
heieidi <heidi.p.hennessy@gmail.com>
2019-11-20 13:40:42 +0200

    TopicForm luotu, POST toimii ja tallettaa tietokantaan. Home-komponentti alustettu, muttei vielä sisältöä

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import React from "react";
 import "./App.css";
 import Navigation from "./layout/Navigation";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import TopicForm from './components/TopicForm';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Navigation title={"Koodifoorumi"} />
-        <Switch>
+ {/*        <Switch>
           <Route path="/" exact component={Home}></Route>
-          <Route path="/topics" exact component={TopicList}></Route>
-          <Route path="/topicform" component={TopicForm}></Route>
-          <Route path="/topics/:id" component={SingleTopic}></Route>
+          <Route path="/topics" exact component={TopicList}></Route> */}
+          <Route path="/topicsform" component={TopicForm}></Route>
+{/*           <Route path="/topics/:id" component={SingleTopic}></Route>
           <Route component={NotFound}></Route>
-        </Switch>
+        </Switch> */}
       </Router>
     </div>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,1 +1,12 @@
 // Index-sivu
+import React, { Component } from 'react'
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
