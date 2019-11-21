07ca26e5438e83b1d5ce7654cc1aab82e261d28d
laukempp <laukempp@gmail.com>
2019-11-21 13:25:25 +0200

    SingleTopic-näkymää muokattu

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ function App() {
           <Route path="/topicsform" component={TopicForm}></Route>
 
           <Route path="/topics/:id" component={SingleTopic}></Route>
-          <Route component={NotFound}></Route>
-         <Route path="/topics/:id" component={SingleTopic}></Route>
-          <Route component={NotFound}></Route>
+          {/* <Route component={NotFound}></Route>
+          <Route path="/topics/:id" component={SingleTopic}></Route>
+          <Route component={NotFound}></Route> */}
         </Switch>
       </Router>
     </div>
